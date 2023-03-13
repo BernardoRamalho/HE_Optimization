12f89e62362c4f954c12c533dbdb3447e4ae7db2
Ramalho <bernardoramalho99@gmail.com>
2023-03-13 17:47:26 +0000

    Added coefficient packed mean calculation

--- Mean/simple-coef-mean.cpp
+++ Mean/simple-coef-mean.cpp
@@ -1,4 +1,120 @@
+#include "openfhe.h"
+#include <iostream>
+#include <fstream>
 
+using namespace lbcrypto;
+/*
+ * argv[1] --> number's file name
+*/
+int main(int argc, char *argv[]) {
 
+    std::ifstream numbers_file (argv[1]);
 
+     if (!numbers_file.is_open()) {
+        std::cerr << "Could not open the file - '"
+             << argv[1] << "'" << std::endl;
+        return EXIT_FAILURE;
+    }
 
+    int64_t number_vectors, size_vectors, number;
+    std::vector<int64_t> all_numbers;
+
+    numbers_file >> number_vectors;
+    numbers_file >> size_vectors;
+    
+    int64_t total_elements = size_vectors * number_vectors;
+
+    while (numbers_file >> number) {
+        all_numbers.push_back(number);
+    }
+
+    TimeVar t;
+    std::vector<double> processingTimes = {0.0, 0.0, 0.0, 0.0, 0.0};
+
+    TIC(t);
+
+    // Sample Program: Step 1: Set CryptoContext
+    CCParams<CryptoContextBFVRNS> parameters;
+    parameters.SetPlaintextModulus(65537);
+    parameters.SetMultiplicativeDepth(2);
+
+    CryptoContext<DCRTPoly> cryptoContext = GenCryptoContext(parameters);
+    // Enable features that you wish to use
+    cryptoContext->Enable(PKE);
+    cryptoContext->Enable(KEYSWITCH);
+    cryptoContext->Enable(LEVELEDSHE);
+    cryptoContext->Enable(ADVANCEDSHE);
+
+    // Sample Program: Step 2: Key Generation
+
+    // Initialize Public Key Containers
+    KeyPair<DCRTPoly> keyPair;
+
+    // Generate a public/private key pair
+    keyPair = cryptoContext->KeyGen();
+
+    // Generate the relinearization key
+    cryptoContext->EvalMultKeyGen(keyPair.secretKey);
+    
+    TOC(t);
+    processingTimes[0] = TOC(t);
+    
+    std::cout << "Duration of setup: " << processingTimes[0] << "ms" << std::endl;
+
+    TIC(t);
+
+    // Create Plaintexts
+    std::vector<Ciphertext<DCRTPoly>> ciphertexts;
+
+    for(unsigned int i = 0; i < all_numbers.size(); i++){
+        std::vector<int64_t> v = {all_numbers[i]};
+        Plaintext plaintext = cryptoContext->MakeCoefPackedPlaintext(v);
+        ciphertexts.push_back(cryptoContext->Encrypt(keyPair.publicKey, plaintext));
+    }
+    
+    TOC(t);
+    processingTimes[1] = TOC(t);
+ 
+    std::cout << "Duration of encryption: " << processingTimes[1] << "ms" << std::endl;
+    
+    TIC(t);
+	    
+    // Homomorphic Operations 
+    auto ciphertextAdd = cryptoContext->EvalAddMany(ciphertexts);
+
+    TOC(t);
+    processingTimes[2] = TOC(t);
+ 
+    std::cout << "Duration of homomorphic operations: " << processingTimes[2] << "ms" << std::endl;
+    
+    TIC(t);
+
+    // Decryption
+    Plaintext plaintextDecAdd;
+ 
+    cryptoContext->Decrypt(keyPair.secretKey, ciphertextAdd, &plaintextDecAdd);
+    plaintextDecAdd->SetLength(size_vectors);
+
+    TOC(t);
+    processingTimes[3] = TOC(t);
+ 
+    std::cout << "Duration of decryption: " << processingTimes[3] << "ms" << std::endl;
+    
+    TIC(t);
+
+    // Plaintext Operations
+    double mean_sum = plaintextDecAdd->GetCoefPackedValue()[0];
+
+    double mean = mean_sum / total_elements; 
+
+    TOC(t);
+    processingTimes[4] = TOC(t);
+ 
+    std::cout << "Duration of plaintext operations: " << processingTimes[4] << "ms" << std::endl;
+    
+    double total_time = std::reduce(processingTimes.begin(), processingTimes.end());
+
+    std::cout << "Total runtime: " << total_time << "ms" << std::endl;
+    std::cout << "Mean: " << mean << std::endl;
+    return 0;
+}
