17bbf31fad95d64d224ef5ff33b726bf70069078
BernardoRamalho <bernardoramalho99@gmail.com>
2023-04-05 14:47:38 +0100

    Added comments to test files

--- tests/rotationCoefTest.cpp
+++ tests/rotationCoefTest.cpp
@@ -3,11 +3,9 @@
 #include <fstream>
 
 using namespace lbcrypto;
-/*
- * argv[1] --> number's file name
-*/
-int main() {
 
+int main() {
+// Read the vector from a file
     std::ifstream numbers_file ("number.txt");
 
      if (!numbers_file.is_open()) {
@@ -16,19 +14,22 @@ int main() {
         return EXIT_FAILURE;
     }
 
+    // Header of file contains information about nr of vector and the size of each of them
     int64_t number_vectors, size_vectors, number;
     std::vector<int64_t> all_numbers;
 
     numbers_file >> number_vectors;
     numbers_file >> size_vectors;
 
+    int64_t total_elements = size_vectors * number_vectors;
 
+    // Body of the file contains all the numbers
     while (numbers_file >> number) {
         all_numbers.push_back(number);
     }
 
  
-    // Sample Program: Step 1: Set CryptoContext
+    // Set CryptoContext
     CCParams<CryptoContextBFVRNS> parameters;
     parameters.SetPlaintextModulus(65537);
     parameters.SetMultiplicativeDepth(2);
@@ -43,7 +44,7 @@ int main() {
     std::cout << "n = " << cryptoContext->GetCryptoParameters()->GetElementParams()->GetCyclotomicOrder()/2 << std::endl;
     std::cout << "log2 q = " << log2(cryptoContext->GetCryptoParameters()->GetElementParams()->GetModulus().ConvertToDouble()) << std::endl;
  
-    // Sample Program: Step 2: Key Generation
+    // Key Generation
 
     // Initialize Public Key Containers
     KeyPair<DCRTPoly> keyPair;
@@ -57,9 +58,15 @@ int main() {
     // Generate rotation vectors
     std::vector<Ciphertext<DCRTPoly>> rotation_ciphertexts;
     Plaintext plaintextRot;
+
     for(int i = 0; i < 13; i++){
-	std::vector<int64_t> rotationVector(8191, 0);
-	rotationVector[(int)pow(2, i)] = 1;
+        // Create vector of size 8192 filled with 0
+        std::vector<int64_t> rotationVector(8191, 0);
+
+        // Rotating by 2^i --> element @ index 2^i = 1
+        rotationVector[(int)pow(2, i)] = 1;
+
+        // Encrypt using Coefficient Packing
         plaintextRot = cryptoContext->MakeCoefPackedPlaintext(rotationVector);
         rotation_ciphertexts.push_back(cryptoContext->Encrypt(keyPair.publicKey, plaintextRot));
     }
@@ -70,34 +77,43 @@ int main() {
     int begin, end;
     
     for(int i = 0; i < number_vectors; i++){
+        // Calculate beginning and end of plaintext values
         begin = i * size_vectors;
         end = size_vectors * (i + 1);
 
+        // Encode Plaintext  with coefficient packing and encrypt it into a ciphertext vector
         Plaintext plaintext = cryptoContext->MakeCoefPackedPlaintext(std::vector<int64_t>(all_numbers.begin() + begin, all_numbers.begin() + end));
         ciphertexts.push_back(cryptoContext->Encrypt(keyPair.publicKey, plaintext));
     }
 
     // Homomorphic Operations 
     auto ciphertextAdd = cryptoContext->EvalAddMany(ciphertexts);
-       // Rotate by 2
-    Plaintext plaintextDecAdd;
+    
+    // Save the result of the addiction into a .txt file
+    // Open the file
     std::string filePath;
 
     std::ofstream out1("coefRotResults/coefRotAdd.txt");
     std::cout.rdbuf(out1.rdbuf()); //redirect std::cout to out.txt!
-
+    
+    // Decrypt and save it into a file
+    Plaintext plaintextDecAdd;
     cryptoContext->Decrypt(keyPair.secretKey, ciphertextAdd, &plaintextDecAdd);
  
     std::cout << plaintextDecAdd->GetCoefPackedValue() << std::endl;
     out1.close();
 
-
+    // Rotate 12 times and each time save the result into a .txt file
     for(int i = 0; i < 13; i++){
+       // Rotate using multiplication
        auto rotCipher = cryptoContext->EvalMult(ciphertextAdd, rotation_ciphertexts[i]);
+       
+       // Open the .txt file
        filePath = "coefRotResults/coefRot" + std::to_string((int)pow(2, i)) + ".txt";   
        std::ofstream out2(filePath);
        std::cout.rdbuf(out2.rdbuf()); //redirect std::cout to out.txt!
-
+       
+       // Decrypt and save the result into a file
        cryptoContext->Decrypt(keyPair.secretKey, rotCipher, &plaintextDecAdd);
  
        std::cout << plaintextDecAdd->GetCoefPackedValue() << std::endl;
