744e161360036827dea719cb768fba9644c13585
BernardoRamalho <bernardoramalho99@gmail.com>
2023-05-24 14:39:24 +0100

    Create subdirectories for each packing type

--- auxiliaryFunctions.h
+++
@@ -1,7 +0,0 @@
-#include "openfhe.h"
-
-std::vector<int64_t> pre_process_numbers(std::vector<int64_t> values, int64_t alpha, int64_t plaintext_modulus);
-
-std::vector<int64_t> post_process_numbers(std::vector<int64_t> pre_processed_values, int64_t alpha, int64_t plaintext_modulus);
-
-std::vector<Plaintext> generate_rotation_plaintexts(int64_t number_rotations);
