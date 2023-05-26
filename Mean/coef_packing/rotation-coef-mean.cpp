365621eea0ca76114542b63ac4addcb972ef9547
BernardoRamalho <bernardoramalho99@gmail.com>
2023-05-26 22:24:03 +0100

    Added n rotations algorithm

---
+++ Mean/coef_packing/rotation-coef-mean.cpp
@@ -0,0 +1,188 @@
+/**
+ * @file optimized-coef-rotation-mean.cpp
+ * @author Bernardo Ramalho
+ * @brief Optimized FHE implementation of the mean of n values using Coefficient Packing
+ * @version 0.1
+ * @date 2023-04-05
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+#include "openfhe.h"
+#include <iostream>
+#include <fstream>
+
+using namespace lbcrypto;
+
+void printIntoCSV(std::vector<double> processingTimes, double total_time, double mean){
+    // Open the file
+    std::string filePath;
+
+    std::ofstream meanCSV("timeCSVs/mean.csv", std::ios_base::app);
+    
+    meanCSV << "coef-rot, ";
+
+    for(unsigned int i = 0; i < processingTimes.size(); i++){
+        meanCSV << processingTimes[i] << ", ";
+    }
+    meanCSV << total_time << ", ";
+    
+    meanCSV << mean << std::endl;
+ 
+    meanCSV.close();
+}
+
+/*
+ * argv[1] --> number's file name
+*/
+int main(int argc, char *argv[]) {
+    // Read the vector from a file
+    std::ifstream numbers_file (argv[1]);
+
+     if (!numbers_file.is_open()) {
+        std::cerr << "Could not open the file - '"
+             << argv[1] << "'" << std::endl;
+        return EXIT_FAILURE;
+    }
+    
+    // Header of file contains information about nr of vector and the size of each of them
+    int64_t number_vectors, size_vectors, number;
+    std::vector<int64_t> all_numbers;
+
+    numbers_file >> number_vectors;
+    numbers_file >> size_vectors;
+
+    int64_t total_elements = size_vectors * number_vectors;
+
+    // Body of the file contains all the numbers
+    while (numbers_file >> number) {
+        all_numbers.push_back(number);
+    }
+
+    TimeVar t;
+    std::vector<double> processingTimes = {0.0, 0.0, 0.0, 0.0, 0.0};
+
+    TIC(t);
+
+    // Set CryptoContext
+    CCParams<CryptoContextBFVRNS> parameters;
+    parameters.SetPlaintextModulus(65537);
+    parameters.SetMultiplicativeDepth(2);
+
+    CryptoContext<DCRTPoly> cryptoContext = GenCryptoContext(parameters);
+    // Enable features that you wish to use
+    cryptoContext->Enable(PKE);
+    cryptoContext->Enable(KEYSWITCH);
+    cryptoContext->Enable(LEVELEDSHE);
+    cryptoContext->Enable(ADVANCEDSHE);
+
+    // Key Generation
+
+    // Initialize Public Key Containers
+    KeyPair<DCRTPoly> keyPair;
+
+    // Generate a public/private key pair
+    keyPair = cryptoContext->KeyGen();
+
+    // Generate the relinearization key
+    cryptoContext->EvalMultKeyGen(keyPair.secretKey);
+    
+    // Generate the rotation plaintexts
+    std::vector<Plaintext> rotation_plaintexts;
+    Plaintext plaintextRot;
+
+    for(int i = 0; i < size_vectors; i++){
+	    // Create vector of size 8192 filled with 0
+        std::vector<int64_t> rotationVector(8191, 0);
+
+        // Rotating by 2^i --> element @ index 2^i = 1
+	    rotationVector[(int)pow(2, i)] = 1;
+
+        rotation_plaintexts.push_back(cryptoContext->MakeCoefPackedPlaintext(rotationVector));
+    }
+
+    
+    // Print time spent on setup
+    TOC(t);
+    processingTimes[0] = TOC(t);
+    
+    //std::cout << "Duration of setup: " << processingTimes[0] << "ms" << std::endl;
+
+    TIC(t);
+
+    // Create Plaintexts
+    std::vector<Ciphertext<DCRTPoly>> ciphertexts;
+    
+    int begin, end;
+    
+    for(int i = 0; i < size_vectors; i++){
+        // Calculate beginning and end of plaintext values
+        begin = i * size_vectors;
+        end = size_vectors * (i + 1);
+
+        // Encode Plaintext  with coefficient packing and encrypt it into a ciphertext vector
+        Plaintext plaintext = cryptoContext->MakeCoefPackedPlaintext(std::vector<int64_t>(all_numbers.begin() + begin, all_numbers.begin() + end));
+        ciphertexts.push_back(cryptoContext->Encrypt(keyPair.publicKey, plaintext));
+    }
+
+    // Print time spent on encryption
+    TOC(t);
+    processingTimes[1] = TOC(t);
+ 
+    //std::cout << "Duration of encryption: " << processingTimes[1] << "ms" << std::endl;
+    
+    TIC(t);
+	    
+    // Homomorphic Operations 
+    auto ciphertextAdd = cryptoContext->EvalAddMany(ciphertexts);
+    auto ciphertextRot = ciphertextAdd;
+    
+    // For each iteration, rotate the vector through multiplication and then add it with the non rotated vector
+    for(int i = 0; i < number_rotations; i++){
+   	    ciphertextRot = cryptoContext->EvalMult(ciphertextAdd, rotation_plaintexts[i]);
+        ciphertextAdd = cryptoContext->EvalAdd(ciphertextAdd, ciphertextRot);
+    }
+
+    // Print time spent on homomorphic operations
+    TOC(t);
+    processingTimes[2] = TOC(t);
+ 
+    //std::cout << "Duration of homomorphic operations: " << processingTimes[2] << "ms" << std::endl;
+    
+    TIC(t);
+
+    // Decryption
+    Plaintext plaintextDecAdd;
+ 
+    cryptoContext->Decrypt(keyPair.secretKey, ciphertextAdd, &plaintextDecAdd);
+
+    // Print time spent on decryption
+    TOC(t);
+    processingTimes[3] = TOC(t);
+ 
+    //std::cout << "Duration of decryption: " << processingTimes[3] << "ms" << std::endl;
+    
+    TIC(t);
+
+    // Plaintext Operations
+    int numberValues = plaintextDecAdd->GetCoefPackedValue().size();
+    
+    double mean_sum = plaintextDecAdd->GetCoefPackedValue()[0]*-1 + plaintextDecAdd->GetCoefPackedValue()[numberValues - 1];
+   
+    double mean = mean_sum / total_elements; 
+
+    // Print time spent on plaintext operations
+    TOC(t);
+    processingTimes[4] = TOC(t);
+ 
+    //std::cout << "Duration of plaintext operations: " << processingTimes[4] << "ms" << std::endl;
+    
+    // Calculate and print final time and value
+    double total_time = std::reduce(processingTimes.begin(), processingTimes.end());
+
+    //std::cout << "Total runtime: " << total_time << "ms" << std::endl;
+    //std::cout << "Mean: " << mean << std::endl;
+
+    printIntoCSV(processingTimes, total_time, mean);
+}
