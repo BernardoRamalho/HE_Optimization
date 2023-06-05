b87e76fbc50411e739940f89c5c89fac259e115a
Ramalho <bernardoramalho99@gmail.com>
2023-06-05 22:37:05 +0000

    Square sum tests concludes that algorithm working with pre processing

--- tests/squareCoef.cpp
+++ tests/squareCoef.cpp
@@ -98,35 +98,46 @@ int main(int argc, char *argv[]) {
    // int64_t alpha = 81, inverse_alpha = 8091;
 	
     std::vector<int64_t> multiply_by(8192, 0);
+    multiply_by[0] = 8192 * 8192;
     std::vector<int64_t> all_numbers(8192, 0);
-    multiply_by[0] = 3;
+    std::vector<int64_t> all_numbers_reversed(8192, 0);
     std::vector<int64_t> all_ones(8192, 1);
-    std::vector<int64_t> all_zeros(8192, 0);
     
    for(int i = 0; i < atoi(argv[1]); i++){
 	   all_numbers[i] = 1;
+	   all_numbers_reversed[i] = 1;
    }
 
+   reverse(all_numbers_reversed.begin(), all_numbers_reversed.end());
+
     std::vector<int64_t> pre_processed_numbers = pre_process_numbers(all_numbers, alpha, plaintext_modulus);
+    std::vector<int64_t> pre_processed_reversed_numbers = pre_process_numbers(all_numbers_reversed, alpha, plaintext_modulus);
     std::vector<int64_t> pre_processed_multiply_by = pre_process_numbers(multiply_by, alpha, plaintext_modulus);
     std::vector<int64_t> pre_processed_all_ones = pre_process_numbers(all_ones, alpha, plaintext_modulus);
-    std::vector<int64_t> pre_processed_all_zeros = pre_process_numbers(all_zeros, alpha, plaintext_modulus);
 
     Plaintext plaintext = cryptoContext->MakeCoefPackedPlaintext(pre_processed_numbers);
+    Plaintext reveserd_plaintext = cryptoContext->MakeCoefPackedPlaintext(pre_processed_reversed_numbers);
     Plaintext plaintextMultiply = cryptoContext->MakeCoefPackedPlaintext(pre_processed_multiply_by);
     Plaintext plaintextAllOnes = cryptoContext->MakeCoefPackedPlaintext(pre_processed_all_ones);
-    Plaintext plaintextAllZeros = cryptoContext->MakeCoefPackedPlaintext(pre_processed_all_zeros);
 
-    //Plaintext plaintext = cryptoContext->MakeCoefPackedPlaintext(all_numbers);
+    // Calculate square sum
     auto elementsCipher = cryptoContext->Encrypt(keyPair.publicKey, plaintext);
     auto ciphertextAdd = cryptoContext->EvalMult(elementsCipher, plaintextAllOnes);
 
-    ciphertextAdd = cryptoContext->EvalMult(ciphertextAdd, plaintextMultiply);
+    ciphertextAdd = cryptoContext->EvalMult(ciphertextAdd, ciphertextAdd);
+
+    // Calculate Inner Product
+     auto elementsReversedCipher = cryptoContext->Encrypt(keyPair.publicKey, plaintext);
+     auto cipherInnerProduct = cryptoContext->EvalMult(elementsCipher, elementsReversedCipher) ;
+
+    cipherInnerProduct = cryptoContext->EvalMult(cipherInnerProduct, plaintextMultiply);
+    
+    cipherInnerProduct = cryptoContext->EvalSub(cipherInnerProduct, ciphertextAdd);
     // Decryption
     Plaintext plaintextDec;
  
-    cryptoContext->Decrypt(keyPair.secretKey, ciphertextAdd, &plaintextDec);
+    cryptoContext->Decrypt(keyPair.secretKey, cipherInnerProduct, &plaintextDec);
     std::vector<int64_t> post_processed_values = post_process_numbers(plaintextDec->GetCoefPackedValue(), inverse_alpha, plaintext_modulus);
-
-    std::cout << post_processed_values << std::endl;
+    float variance = post_processed_values[atoi(argv[1])-1] / pow(8192, 3);
+    std::cout << variance  << std::endl;
 }
