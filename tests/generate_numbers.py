2b2a5a5037fe701a060ebb48025d327d07172080
BernardoRamalho <bernardoramalho99@gmail.com>
2023-06-14 15:03:21 +0100

    Lacks Security Implication outcomes

--- tests/generate_numbers.py
+++ tests/generate_numbers.py
@@ -1,22 +1,34 @@
+import numpy as np
+
 vector_size = int(input("Which is the vector size? "))
 number_vectors = int(input("How many vectors?"))
-
-f = open("number.txt", "w")
+total_elements = vector_size * number_vectors
+all_numbers = []
 number = 1
 sum_all_numbers = 0
-f.write(str(number_vectors) + " ")
-f.write(str(vector_size) + "\n")
 
 for i in range(number_vectors):
     for j in range(vector_size):
-        #if j < 10:
-        #    f.write("1 ")
-        #else:
-        #    f.write("0 ")
-        f.write(str(number) + " ")
+        all_numbers.append(number)
         sum_all_numbers += number
         number = (number + 1) % 3
     number = 1
-    f.write("\n")
-f.close()
-print("Sum of all numbers = " + str(sum_all_numbers))
+
+all_numbers = np.array(all_numbers) * total_elements
+all_numbers = all_numbers - sum_all_numbers
+all_numbers = all_numbers * all_numbers
+
+all_numbers = [int(x) for x in all_numbers]
+print(sum(all_numbers))
+
+#inner_product = int(np.inner(all_numbers, all_numbers))
+#n_squared = total_elements ** 2
+
+#squared_inner = inner_product * n_squared
+
+#print("n^3 = " + str(pow(total_elements, 3)))
+#print("Sum of all numbers = " + str(sum_all_numbers))
+#print("Square Sum of all numbers = " + str(pow(sum_all_numbers, 2)))
+#print("n x Square Sum of all numbers = " + str( total_elements * pow(sum_all_numbers, 2)))
+#print("n x Inner Product = " + str(total_elements * inner_product)) 
+#print("n^2 x Inner Product = " + str(squared_inner)) 
--- tests/prime.py
+++ tests/prime.py
@@ -1,7 +1,8 @@
 from sympy import *
 
 n = 7000000189057
-m = 732962996176 
+m = 2.29093 * 10**17
+ 
 while(true):
     m +=1
     if(isprime(m) and ((m - 1) / 16384).is_integer()):
